3b5ba33158e30174ec0b4a2accc871f8441cf201
Aayush-pathak <160565771+Aayush-pathak777@users.noreply.github.com>
2024-09-07 09:38:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 def send_whatsapp_message():
-    pip install pywhatkit
     import pywhatkit
     phone_number = '+91' + input('Enter your phone number: ')
     message = input('Enter your message: ')
